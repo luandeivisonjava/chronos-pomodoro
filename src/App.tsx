12a85ca84042261d25016189c17d2869f7accc45
luandeivisonjava <luandeivison.java@gmail.com>
2025-06-30 04:36:07 -0300

    criando os componentes de menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Heading } from "./components/Heading";
-import { TimerIcon } from "lucide-react";
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+
 
 export function App() {
 
   return (
-    <div>
-      <Heading>
-        Hello World
-        <button><TimerIcon /></button>
-        </Heading>
-    </div>
+   <>
+   <Container>
+    <Logo />
+   </Container>
+   <Container>
+    <Menu />
+   </Container>
+   </>
   )
 };
